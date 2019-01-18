0bf89498fa4cef7fc1df57a3c0ce76d0c3cc5450
$€ß∀$ <sebas@localhost.localdomain>
2019-01-18 10:09:18 +0100

    Corregidos fallos metodos base datos

--- DAMTweet/src/logica/DBHelper.java
+++ DAMTweet/src/logica/DBHelper.java
@@ -12,6 +12,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,49 +24,23 @@ public class DBHelper {
     // this.pathCarpetaPrograma = System.getProperty("user.home")+File.separator+"." + pathCarpetaPrograma;
     String pathCarpetaPrograma;
     String dbName;
-    Connection conn;
-    
-    
+    String urlDBFile;
 
     public DBHelper(String pathCarpetaPrograma, String dbName) {
         this.pathCarpetaPrograma = pathCarpetaPrograma;
         this.dbName = dbName;
-        this.conn = null;
-        crearConexionDb();
-
+        this.urlDBFile = "jdbc:sqlite:" + pathCarpetaPrograma + File.separator + dbName;
+        crearTablas();
     }
 
-    private void crearConexionDb() {
+    private Connection connect() {
+        Connection conn = null;
         try {
-            
-
-            String url = "jdbc:sqlite:" + pathCarpetaPrograma + File.separator + dbName;
-            System.out.println(url);
-
-            this.conn = DriverManager.getConnection(url);
-
-            System.out.println("Connexiuon SQLite establecida.");
-
+            conn = DriverManager.getConnection(urlDBFile);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
-    }
-
-    public void cerrarConexionDb() {
-        try {
-            conn.close();
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-
+        return conn;
     }
 
     public void crearTablas() {
@@ -72,52 +48,63 @@ public class DBHelper {
         /* Instruciones SQL para crear la tabla usuarios, posiblemente en el futuro 
         habra que crear mas tablas y normaliuzarlas, crear relaciones etc..               
          */
-        String sql = "CREATE TABLE IF NOT EXISTS users (\n"
+        String sql = "CREATE TABLE IF NOT EXISTS USERS (\n"
                 + "	id integer PRIMARY KEY,\n"
                 + "	name text NOT NULL,\n"
-                + "	user_name text NOT NULL,\n"
-                + "     token text NOT NULL\n"
+                + "	user_name text NOT NULL UNIQUE,\n"
+                + "     token text NOT NULL,\n"
                 + "     secret_token text NOT NULL\n"
                 + ");";
 
-        try {
-            Statement stmt = conn.createStatement();
+        try (Connection conn = this.connect();
+                Statement stmt = conn.createStatement()) {
+
             stmt.execute(sql);
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
 
     }
 
     public void insertarUsuario(String name, String user_name, String token, String secret_token) {
-        
-        String sql = "INSERT INTO users(name, user_name, token, secret_token) VALUES(?,?,?,?)";
 
-        try {
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+        String sql = "INSERT INTO USERS(name, user_name, token, secret_token) VALUES(?,?,?,?)";
+
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, name);
             pstmt.setString(2, user_name);
             pstmt.setString(3, token);
             pstmt.setString(4, secret_token);
 
             pstmt.executeUpdate();
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+
     }
 
     public ResultSet getUserData(String user_name) {
-        String sql = "SELECT * FROM users WHERE user_name=?";
-      
-        try {
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            // set the value
+        String sql = "SELECT * FROM USERS WHERE user_name=?";
+
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
             pstmt.setString(1, user_name);
-            ResultSet rs = pstmt.executeQuery(sql);
-            return rs;
+
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t"
+                        + rs.getString("name") + "\t"
+                        + rs.getDouble("user_name"));
+            }
+
         } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+            Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         return null;
     }
 
